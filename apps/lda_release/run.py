9fe522de04e2c2e1b60c2da9365cd3f5fe373a46
Jin Kyu Kim <jinkyuk@h0.ub16strads.biglearning.orca.pdl.cmu.edu>
2017-03-24 10:14:03 -0600

    bug fix patch

--- apps/lda_release/run.py
+++ apps/lda_release/run.py
@@ -7,7 +7,11 @@ topics = [' 1000 ']
 iterations = [' 3 ']
 threads = [' 16 ']
 
+
 machfile = ['./singlemach.vm']
+nodes=['6']
 
 prog = ['./bin/ldall ']
-os.system("mpirun -machinefile "+machfile[0]+" "+prog[0]+" --machfile "+machfile[0]+" -threads "+threads[0]+" -num_topic "+topics[0]+" -num_iter "+iterations[0]+" -data_file "+datafile[0]+" -logfile tmplog/1 -wtfile_pre tmplog/wt -dtfile_pre tmplog/dt ");
+os.system("mpirun -machinefile "+machfile[0]+" --display-map --bynode -np "+nodes[0]+" "+prog[0]+" --machfile "+machfile[0]+" -threads "+threads[0]+" -num_topic "+topics[0]+" -num_iter "+iterations[0]+" -data_file "+datafile[0]+" -logfile tmplog/1 -wtfile_pre tmplog/wt -dtfile_pre tmplog/dt ");
+
+#os.system("mpirun -machinefile "+machfile[0]+" --display-map --bynode -np "+nodes[0]+" "+prog[0]+" --machfile "+machfile[0]+" -threads "+threads[0]+" -num_topic "+topics[0]+" -num_iter "+iterations[0]+" -data_file "+datafile[0]+" -logfile tmplog/1 -wtfile_pre tmplog/wt -dtfile_pre tmplog/dt ");
--- apps/linear-solver_release/lasso.py
+++ apps/linear-solver_release/lasso.py
@@ -2,7 +2,8 @@
 import os
 import sys
 
-machfile = ['./singlemach.vm']
+machfile = ['./multi.vm']
+nodes=['6']
 
 # data setting 
 datafilex = ['./input/lasso500by1K.X.mmt']
@@ -22,4 +23,4 @@ set_size = [' 1 ']
 
 prog = ['./bin/cdsolver ']
 
-os.system(" mpirun -machinefile "+machfile[0]+" "+prog[0]+" --machfile "+machfile[0]+" -threads "+worker_threads[0]+" -samples "+csample[0]+" -columns "+column[0]+" -data_xfile "+datafilex[0]+" -data_yfile "+datafiley[0]+"  -schedule_size "+set_size[0]+" -max_iter 30000 -lambda 0.001 -scheduler "+cscheduler[0]+" -threads_per_scheduler "+scheduler_threads[0]+" -weight_sampling=false -check_interference=false -algorithm lasso");
+os.system(" mpirun -machinefile "+machfile[0]+" --display-map --bynode -np "+nodes[0]+" "+prog[0]+" --machfile "+machfile[0]+" -threads "+worker_threads[0]+" -samples "+csample[0]+" -columns "+column[0]+" -data_xfile "+datafilex[0]+" -data_yfile "+datafiley[0]+"  -schedule_size "+set_size[0]+" -max_iter 30000 -lambda 0.001 -scheduler "+cscheduler[0]+" -threads_per_scheduler "+scheduler_threads[0]+" -weight_sampling=false -check_interference=false -algorithm lasso");
